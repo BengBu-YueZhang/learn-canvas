2aade3afeb088e7259c9574ef6cc1408921cfa4a
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-24 13:24:18 +0800

    添加笔记

--- README.md
+++ README.md
@@ -850,3 +850,101 @@ let hourhand = new Hourhand(ctx, 0, 0)
 window.requestAnimationFrame(hourhand.draw.bind(hourhand))
 
 ```
+
+#### 爱心闪烁
+
+```js
+
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getContext('2d')
+let hearts = []
+const colors = [
+  '#80d8ff',
+  '#c5e1a5',
+  '#303f9f',
+  '#ff4081',
+  '#d50000',
+  '#8bc34a',
+  '#66bb6a',
+  '#ff9800',
+  '#ffeb3b',
+  '#29b6f6',
+  '#80deea',
+  '#8bc34a',
+  '#eeff41',
+  '#f9a825',
+  '#f4511e',
+  '#ff80ab',
+  '#ad1457',
+  '#ff4081',
+  '#f50057'
+]
+
+class Heart {
+  constructor (color, alpha, x, y) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.alpha = alpha
+    this.status = 'positive'
+  }
+
+  draw () {
+    if (this.status === 'positive') {
+      this.alpha += 0.006
+      if (this.alpha >= 0.9) {
+        this.alpha = 0.9
+        this.status = 'negative'
+      }
+    } else {
+      this.alpha -= 0.006
+      if (this.alpha <= 0) {
+        this.alpha = 0.01
+        this.status = 'positive'
+      }
+    }
+    ctx.save()
+    ctx.translate(
+      this.x,
+      this.y
+    )
+    ctx.globalAlpha = this.alpha
+    ctx.beginPath()
+    ctx.fillStyle = this.color
+    ctx.moveTo(0, 0)
+    ctx.bezierCurveTo(-30, -10, -20, 30, 0, 30)
+    ctx.bezierCurveTo(20, 30, 30, -10, 0, 0)
+    ctx.fill()
+    ctx.restore()
+  }
+}
+
+for (let i = 0; i < 100; i++) {
+  let color = colors[
+    Math.floor(Math.random() * colors.length)
+  ]
+  let heart = new Heart(
+    color,
+    Math.random(),
+    Math.floor(Math.random() * 500),
+    Math.floor(Math.random() * 500)
+  )
+  hearts.push(heart)
+}
+
+function animation () {
+  ctx.clearRect(0, 0, 500, 500)
+  ctx.save()
+  ctx.fillRect(0, 0, 500, 500)
+  for (let i = 0; i < 100; i++) {
+    hearts[i].draw()
+  }
+  ctx.restore()
+  window.requestAnimationFrame(animation)
+}
+
+
+window.requestAnimationFrame(animation)
+
+
+```
--- index.js
+++ index.js
@@ -1,113 +1,90 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
+let hearts = []
+const colors = [
+  '#80d8ff',
+  '#c5e1a5',
+  '#303f9f',
+  '#ff4081',
+  '#d50000',
+  '#8bc34a',
+  '#66bb6a',
+  '#ff9800',
+  '#ffeb3b',
+  '#29b6f6',
+  '#80deea',
+  '#8bc34a',
+  '#eeff41',
+  '#f9a825',
+  '#f4511e',
+  '#ff80ab',
+  '#ad1457',
+  '#ff4081',
+  '#f50057'
+]
 
-let width = parseInt(canvas.getAttribute('width').replace(/px/, ''), 10)
-let height = parseInt(canvas.getAttribute('height').replace(/px/, ''), 10)
-
-console.log(width)
-console.log(height)
-
-
-class Hourhand {
-  constructor (ctx) {
-    this.ctx = ctx
-    this.date = new Date()
+class Heart {
+  constructor (color, alpha, x, y) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.alpha = alpha
+    this.status = 'positive'
   }
 
   draw () {
-    this.date = new Date()
-    ctx.clearRect(0, 0, width, height)
-    this.drawDial()
-    this.drawScale()
-    this.drawSecond()
-    this.drawMinute()
-    this.drawHour()
-    window.requestAnimationFrame(this.draw.bind(this))
-  }
-
-  drawDial () {
-    ctx.save()
-    ctx.beginPath()
-    ctx.translate(150, 150)
-    ctx.lineWidth = 5
-    ctx.strokeStyle = '#304ffe'
-    ctx.arc(0, 0, 100, 0, (Math.PI/180) * 360, true)
-    ctx.closePath()
-    ctx.stroke()
-    ctx.restore()
-  }
-
-  drawScale () {
-    const drawSecondPoint = (i) => {
-      ctx.save()
-      ctx.beginPath()
-      ctx.translate(150, 150)
-      ctx.lineWidth = 2
-      if (i % 5 === 0) ctx.lineWidth = 6
-      ctx.strokeStyle = '#009688'
-      ctx.rotate((Math.PI/180) * i * 6)
-      if (i % 5 === 0) {
-        ctx.moveTo(0, -85)
-      } else {
-        ctx.moveTo(0, -90)
+    if (this.status === 'positive') {
+      this.alpha += 0.006
+      if (this.alpha >= 0.9) {
+        this.alpha = 0.9
+        this.status = 'negative'
       }
-      ctx.lineTo(0, -98)
-      ctx.closePath()
-      ctx.stroke()
-      ctx.restore()
+    } else {
+      this.alpha -= 0.006
+      if (this.alpha <= 0) {
+        this.alpha = 0.01
+        this.status = 'positive'
+      }
     }
-    for (let i = 0; i < 60; i++) {
-      drawSecondPoint(i)
-    }
-  }
-
-  drawSecond () {
-    let second = this.date.getSeconds()
     ctx.save()
+    ctx.translate(
+      this.x,
+      this.y
+    )
+    ctx.globalAlpha = this.alpha
     ctx.beginPath()
-    ctx.translate(150, 150)
-    ctx.lineWidth = 2
-    ctx.strokeStyle = '#e65100'
-    ctx.rotate((Math.PI/180) * second * 6)
+    ctx.fillStyle = this.color
     ctx.moveTo(0, 0)
-    ctx.lineTo(0, -80)
-    ctx.closePath()
-    ctx.stroke()
-    ctx.restore()
-  }
-
-  drawMinute () {
-    let minute = this.date.getMinutes()
-    ctx.save()
-    ctx.beginPath()
-    ctx.translate(150, 150)
-    ctx.lineWidth = 8
-    ctx.strokeStyle = '#1b5e20'
-    ctx.rotate((Math.PI/180) * minute * 6)
-    ctx.moveTo(0, 0)
-    ctx.lineTo(0, -50)
-    ctx.closePath()
-    ctx.stroke()
-    ctx.restore()
-  }
-
-  drawHour () {
-    let hour = this.date.getHours()
-    hour = hour < hour ? hour : hour - 12
-    ctx.save()
-    ctx.beginPath()
-    ctx.translate(150, 150)
-    ctx.lineWidth = 8
-    ctx.strokeStyle = '#0091ea'
-    ctx.rotate((Math.PI/180) * hour * 30)
-    ctx.moveTo(0, 0)
-    ctx.lineTo(0, -25)
-    ctx.closePath()
-    ctx.stroke()
+    ctx.bezierCurveTo(-30, -10, -20, 30, 0, 30)
+    ctx.bezierCurveTo(20, 30, 30, -10, 0, 0)
+    ctx.fill()
     ctx.restore()
   }
 }
 
-let hourhand = new Hourhand(ctx, 0, 0)
+for (let i = 0; i < 100; i++) {
+  let color = colors[
+    Math.floor(Math.random() * colors.length)
+  ]
+  let heart = new Heart(
+    color,
+    Math.random(),
+    Math.floor(Math.random() * 500),
+    Math.floor(Math.random() * 500)
+  )
+  hearts.push(heart)
+}
 
-window.requestAnimationFrame(hourhand.draw.bind(hourhand))
+function animation () {
+  ctx.clearRect(0, 0, 500, 500)
+  ctx.save()
+  ctx.fillRect(0, 0, 500, 500)
+  for (let i = 0; i < 100; i++) {
+    hearts[i].draw()
+  }
+  ctx.restore()
+  window.requestAnimationFrame(animation)
+}
+
+
+window.requestAnimationFrame(animation)

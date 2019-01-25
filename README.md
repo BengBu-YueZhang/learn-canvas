d5ace83b82b483231955d8dfe768cdaf116ed76a
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-25 17:06:29 +0800

    添加笔记

--- README.md
+++ README.md
@@ -948,3 +948,96 @@ window.requestAnimationFrame(animation)
 
 
 ```
+
+### 高级动画
+
+#### 绘制小球
+
+```js
+
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getContext('2d')
+const length = 50
+const width = canvas.width
+const height = canvas.height
+// 小球的集合
+let balls = []
+
+class Ball {
+  constructor (x, y, vx, vy, index, size = 2, color = '#757575') {
+    this.x = x
+    this.y = y
+    this.vx = vx
+    this.vy = vy
+    this.size = size
+    this.color = color
+    this.index = index
+  }
+
+  draw () {
+    ctx.save()
+    ctx.fillStyle = this.color
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, true)
+    this.drawIine()
+    if (this.x >= width - this.size || this.x <= 0 + this.size) {
+      this.vx = -this.vx
+    }
+    if (this.y >= height - this.size || this.y <= 0 + this.size) {
+      this.vy = -this.vy
+    }
+    this.x += this.vx
+    this.y += this.vy
+    ctx.fill()
+    ctx.beginPath()
+    ctx.restore()
+  }
+
+  drawIine () {
+    for (let i = 0; i < balls.length; i++) {
+      if (i === this.index) {
+        continue
+      } else {
+        let diffx = Math.abs(this.x - balls[i].x)
+        let diffy = Math.abs(this.y - balls[i].y)
+        let diffs = Math.sqrt((diffx * diffx) + (diffy * diffy))
+        if (diffs <= 200 ) {
+          ctx.strokeStyle = `rgba(189, 189, 189, ${diffs / 200})`
+          ctx.moveTo(this.x, this.y)
+          ctx.lineTo(balls[i].x, balls[i].y)
+          ctx.stroke()
+        }
+      }
+    }
+  }
+}
+
+function init () {
+  for (let i = 0; i < length; i++) {
+    let xDirection = Math.floor(Math.random() * 2)
+    let yDirection = Math.floor(Math.random() * 2)
+    let x = Math.floor(Math.random() * width)
+    let y = Math.floor(Math.random() * height)
+    // -20 ~ -10 10 ~ 20的随机值
+    let vx = Math.floor(Math.random() * 2 + 1)
+    let vy = Math.floor(Math.random() * 2 + 1)
+    vx = xDirection === 1 ? vx : -vx
+    vy = yDirection === 1 ? vy : -vy
+    let ball = new Ball(x, y, vx, vy, i)
+    balls.push(ball)
+  }
+}
+
+function draw () {
+  ctx.save()
+  ctx.clearRect(0, 0, width, height)
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw()
+  }
+  ctx.restore()
+  window.requestAnimationFrame(draw)
+}
+
+init()
+
+window.requestAnimationFrame(draw)
+```
--- index.js
+++ index.js
@@ -1,101 +1,85 @@
 let canvas = document.getElementById('canvas')
-canvas.style.filter = `blur(0.3px)`
-canvas.style.overflow = `hidden`
 let ctx = canvas.getContext('2d')
-let hearts = []
-const colors = [
-  '#80d8ff',
-  '#c5e1a5',
-  '#303f9f',
-  '#ff4081',
-  '#d50000',
-  '#8bc34a',
-  '#66bb6a',
-  '#ff9800',
-  '#ffeb3b',
-  '#29b6f6',
-  '#80deea',
-  '#8bc34a',
-  '#eeff41',
-  '#f9a825',
-  '#f4511e',
-  '#ff80ab',
-  '#ad1457',
-  '#ff4081',
-  '#f50057',
-  '#dce775',
-  '#c0ca33',
-  '#2e7d32',
-  '#ff4081',
-  '#e53935',
-  '#9c27b0',
-  '#80d8ff',
-  '#81d4fa',
-  '#ff9100'
-]
+const length = 50
+const width = canvas.width
+const height = canvas.height
+// 小球的集合
+let balls = []
 
-class Heart {
-  constructor (color, alpha, x, y) {
+class Ball {
+  constructor (x, y, vx, vy, index, size = 2, color = '#757575') {
     this.x = x
     this.y = y
+    this.vx = vx
+    this.vy = vy
+    this.size = size
     this.color = color
-    this.alpha = alpha
-    this.status = 'positive'
+    this.index = index
   }
 
   draw () {
-    if (this.status === 'positive') {
-      this.alpha += 0.005
-      if (this.alpha >= 0.9) {
-        this.alpha = 0.9
-        this.status = 'negative'
-      }
-    } else {
-      this.alpha -= 0.005
-      if (this.alpha <= 0) {
-        this.alpha = 0
-        this.status = 'positive'
-      }
-    }
     ctx.save()
-    ctx.translate(
-      this.x,
-      this.y
-    )
-    ctx.globalAlpha = this.alpha
-    ctx.beginPath()
     ctx.fillStyle = this.color
-    ctx.moveTo(0, 0)
-    ctx.bezierCurveTo(-30, -10, -20, 30, 0, 30)
-    ctx.bezierCurveTo(20, 30, 30, -10, 0, 0)
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, true)
+    this.drawIine()
+    if (this.x >= width - this.size || this.x <= 0 + this.size) {
+      this.vx = -this.vx
+    }
+    if (this.y >= height - this.size || this.y <= 0 + this.size) {
+      this.vy = -this.vy
+    }
+    this.x += this.vx
+    this.y += this.vy
     ctx.fill()
+    ctx.beginPath()
     ctx.restore()
   }
+
+  drawIine () {
+    for (let i = 0; i < balls.length; i++) {
+      if (i === this.index) {
+        continue
+      } else {
+        let diffx = Math.abs(this.x - balls[i].x)
+        let diffy = Math.abs(this.y - balls[i].y)
+        let diffs = Math.sqrt((diffx * diffx) + (diffy * diffy))
+        if (diffs <= 200 ) {
+          ctx.strokeStyle = `rgba(189, 189, 189, ${diffs / 200})`
+          ctx.moveTo(this.x, this.y)
+          ctx.lineTo(balls[i].x, balls[i].y)
+          ctx.stroke()
+        }
+      }
+    }
+  }
 }
 
-for (let i = 0; i < 330; i++) {
-  let color = colors[
-    Math.floor(Math.random() * colors.length)
-  ]
-  let heart = new Heart(
-    color,
-    Math.random(),
-    Math.floor(Math.random() * 2560),
-    Math.floor(Math.random() * 1440)
-  )
-  hearts.push(heart)
+function init () {
+  for (let i = 0; i < length; i++) {
+    let xDirection = Math.floor(Math.random() * 2)
+    let yDirection = Math.floor(Math.random() * 2)
+    let x = Math.floor(Math.random() * width)
+    let y = Math.floor(Math.random() * height)
+    // -20 ~ -10 10 ~ 20的随机值
+    let vx = Math.floor(Math.random() * 2 + 1)
+    let vy = Math.floor(Math.random() * 2 + 1)
+    vx = xDirection === 1 ? vx : -vx
+    vy = yDirection === 1 ? vy : -vy
+    let ball = new Ball(x, y, vx, vy, i)
+    balls.push(ball)
+  }
 }
 
-function animation () {
-  ctx.clearRect(0, 0, 2560, 1440)
+function draw () {
   ctx.save()
-  ctx.fillRect(0, 0, 2560, 1440)
-  for (let i = 0; i < hearts.length; i++) {
-    hearts[i].draw()
+  ctx.clearRect(0, 0, width, height)
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw()
   }
   ctx.restore()
-  window.requestAnimationFrame(animation)
+  window.requestAnimationFrame(draw)
 }
 
+init()
 
-window.requestAnimationFrame(animation)
+window.requestAnimationFrame(draw)
